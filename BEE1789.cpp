f3d078a4f865a9376c33aa5940ca7f17ba46561c
João Hypolito <joaohypolito1@gmail.com>
2025-03-23 16:55:34 -0300

    Add files via upload

---
+++ BEE1789.cpp
@@ -0,0 +1,31 @@
+//Ctrl + Z (Windows) e pressionar Enter para finalizar o programa
+#include <stdio.h>
+
+int main(){
+    
+    int L, V, maior;
+
+    while(scanf("%d", &L) != EOF){
+        scanf("%d", &maior);
+
+    for(int i = 1; i < L; i++){
+        scanf("%d", &V);
+        if( V > maior){
+            maior = V;
+        }
+
+    }    
+
+    if(maior < 10){
+        printf("1\n");
+    }else if(maior < 20){
+        printf("2\n");
+    }else if(maior >= 20){
+        printf("3\n");
+    }
+
+    }
+
+
+    return 0;
+}
