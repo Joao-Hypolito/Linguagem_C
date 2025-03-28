df635129b3cb43e23be7611393608cfb312e7201
João Hypolito <joaohypolito1@gmail.com>
2025-03-28 14:14:24 -0300

    Add files via upload

---
+++ BEE2006.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int T, A, B, C, D, E;
+    int acertos = 0;
+
+    scanf("%d", &T);
+    scanf("%d %d %d %d %d", &A, &B, &C, &D, &E);
+
+    if(A == T)acertos++;
+    if(B == T)acertos++;
+    if(C == T)acertos++;
+    if(D == T)acertos++;
+    if(E == T)acertos++;
+
+    printf("%d\n", acertos);
+
+
+
+    return 0;
+}
